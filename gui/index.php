4fd19ac62e534d0640d4e36c6da2f2997ad95c72
PlasmaDan <github@plasmadan.com>
2020-11-02 04:17:11 +0000

    Update
    Improved web application usage for Android + iOS (Add to Home Screen)

--- gui/index.php
+++ gui/index.php
@@ -41,11 +41,14 @@ $root_dir = str_replace($_SERVER['DOCUMENT_ROOT'], '', __DIR__) . '/';
     <link rel="apple-touch-icon" sizes="180x180" href="/src/icons/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/src/icons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/src/icons/favicon-16x16.png">
-    <link rel="manifest" href="/src/icons/site.webmanifest">
     <link rel="mask-icon" href="/src/icons/safari-pinned-tab.svg" color="#5bbad5">
     <link rel="shortcut icon" href="/src/icons/favicon.ico">
     <meta name="msapplication-TileColor" content="#00aba9">
     <meta name="msapplication-config" content="/src/icons/browserconfig.xml">
+    <meta name="application-name" content="<?= $board->board_name ?>">
+    <meta name="apple-mobile-web-app-title" content="<?= $board->board_name ?>">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
     <meta name="theme-color" content="#ffffff">
     
     <!-- Search Engine -->
